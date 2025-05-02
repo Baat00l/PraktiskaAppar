799d6d2c691102a8feb2db032a93c02512e80d42
Batool Fahmi <batool.fahmi.webd24jon@edu.tucsweden.se>
2025-05-02 10:43:07 +0200

    Add project files.

---
+++ Northwind.Mvc/Views/Home/Category.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Northwind.Mvc.Views.Home
+{
+    public class CategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+            ViewComponent("Category");
+        }
+    }
+}
