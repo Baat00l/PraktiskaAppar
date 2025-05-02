0fdf3e8e879a61fb8801a78d3fa1892a3dbf7929
Batool Fahmi <batoolfahmi4@gmail.com>
2025-05-02 10:46:14 +0200

    Delete PraktiskaaAppar directory

--- PraktiskaaAppar/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CustomI.Sample.CustomUserManagement.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
